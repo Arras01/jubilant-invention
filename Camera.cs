24a5e1801e970ff68cad136eda39639b54167432
Jasper Kraimaat <jasper.kraimaat@gmail.com>
2016-02-18 22:40:09 +0100

    Add camera class

---
+++ Camera.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using Template.Objects;
+
+namespace Template
+{
+    class Camera
+    {
+        public Vector3 Position;
+        public Vector3 Direction;
+        public float Distance;
+
+        Vector3 topLeft;
+        Vector3 topRight;
+        Vector3 bottomLeft;
+
+
+        public Camera()
+        {
+            Position = new Vector3();
+            Direction = new Vector3(1, 0, 0);
+            Distance = 1;
+            Vector3 center = Position + Direction * Distance;
+            topLeft = center + new Vector3(-1, -1, 0);
+            topRight = center + new Vector3(1, -1, 0);
+            bottomLeft = center + new Vector3(-1, 1, 0);
+        }
+
+        public void MoveCamera(Matrix4 m)
+        {
+            Position = Vector3.TransformPosition(Position, m);
+            topLeft = Vector3.TransformPosition(topLeft, m);
+            topRight = Vector3.TransformPosition(topRight, m);
+            bottomLeft = Vector3.TransformPosition(bottomLeft, m);
+        }
+
+        public IEnumerable<Ray> GenerateRays(int width, int height)
+        {
+            for (int u = 0; u < width; u++)
+            {
+                for (int v = 0; v < height; v++)
+                {
+                    Vector3 d = topLeft + (float)u / width * (topRight - topLeft) + (float)v / height * (bottomLeft - topLeft);
+                    yield return new Ray(Position, (d - Position).Normalized());
+                }
+            }
+        }
+    }
+}
