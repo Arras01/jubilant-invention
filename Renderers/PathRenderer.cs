fba1f99a762bb3434476d49b9f0e119f22cc1d1a
Jasper Kraimaat <jasper.kraimaat@gmail.com>
2016-07-04 13:40:59 +0200

    add importance sampling?

--- Renderers/PathRenderer.cs
+++ Renderers/PathRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using OpenTK;
 using Template.Objects;
 
@@ -28,12 +29,14 @@ namespace Template
                 return ray.IntersectedMaterial.Color * Trace(rf, ++depth);
             }
 
-            Vector3 R = DiffuseReflection(ray.IntersectionNormal);
-            Vector3 BRDF = ray.IntersectedMaterial.Color / (float)Math.PI;
+            Vector3 R = WeightedDiffuseReflection(ray.IntersectionNormal);
             Ray r = new Ray(ray.IntersectionPoint, R);
+            Vector3 BRDF = ray.IntersectedMaterial.Color / (float)Math.PI;
+            float PDF = (float)(Vector3.Dot(ray.IntersectionNormal, R) / Math.PI);
+            //float PDF = (float) (1 / (2 * Math.PI));
 
-            var Ei = Trace(r, ++depth) * Vector3.Dot(ray.IntersectionNormal, R);
-            return (float)Math.PI * 2f * BRDF * Ei;
+            var Ei = Trace(r, ++depth) * Vector3.Dot(ray.IntersectionNormal, R) / PDF;
+            return BRDF * Ei;
         }
 
         private Ray ReflectRay(Ray r, Vector3 normal)
@@ -42,7 +45,35 @@ namespace Template
             return new Ray(r.Origin + r.Direction * r.NearestIntersection + newDirection * 0.0001f, newDirection);
         }
 
-        private Vector3 DiffuseReflection(Vector3 normal)
+        private Vector3 WeightedDiffuseReflection(Vector3 normal)
+        {
+            Vector3 u, v;
+            if (normal.X > 0.99f)
+            {
+                u = Vector3.Cross(normal, new Vector3(0, 1, 0));
+            }
+            else
+                u = Vector3.Cross(normal, new Vector3(1, 0, 0));
+
+            v = Vector3.Cross(normal, u);
+
+            Vector3 result;
+            var r0 = Random.NextDouble();
+            var r1 = Random.NextDouble();
+            var r = Math.Sqrt(r0);
+            var theta = (2 * Math.PI * r1);
+            var x = r0 * Math.Cos(theta);
+            var y = r * Math.Sin(theta);
+            result = new Vector3((float)x, (float)y, (float)Math.Sqrt(1 - r0));
+
+            var m = new Matrix4(new Vector4(u), new Vector4(v), new Vector4(normal), new Vector4(0, 0, 0, 1));
+
+            var finalresult = Vector3.TransformVector(result, m);
+
+            return finalresult.Normalized();
+        }
+
+        private Vector3 OldDiffuseReflection(Vector3 normal)
         {
             Vector3 result;
             do
